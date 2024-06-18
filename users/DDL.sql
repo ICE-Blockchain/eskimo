21778a5f30f4f35a03e25af2a8be1162405f6c49
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-06-18 09:54:14 +0300

    fix comments

--- users/DDL.sql
+++ users/DDL.sql
@@ -50,11 +50,8 @@ ALTER TABLE users ADD COLUMN IF NOT EXISTS kyc_step_blocked smallint NOT NULL DE
 ALTER TABLE users ADD COLUMN IF NOT EXISTS kyc_steps_last_updated_at timestamp[];
 ALTER TABLE users ADD COLUMN IF NOT EXISTS kyc_steps_created_at timestamp[];
 ALTER TABLE users ADD COLUMN IF NOT EXISTS telegram_user_id text;
-ALTER TABLE users ALTER COLUMN telegram_user_id SET DEFAULT '';
 UPDATE users SET telegram_user_id = id WHERE telegram_user_id IS NULL;
-ALTER TABLE users ALTER COLUMN telegram_user_id SET NOT NULL;
 ALTER TABLE users ADD COLUMN IF NOT EXISTS telegram_bot_id text;
-ALTER TABLE users ALTER COLUMN telegram_bot_id SET DEFAULT '';
 UPDATE users SET telegram_bot_id = id WHERE telegram_bot_id IS NULL;
 ALTER TABLE users ALTER COLUMN telegram_bot_id SET NOT NULL;
 INSERT INTO users (created_at,updated_at,phone_number,phone_number_hash,email,id,username,profile_picture_name,referred_by,city,country,mining_blockchain_account_address,blockchain_account_address, telegram_user_id, lookup)
