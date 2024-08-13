2419d4be7f58eebf3e8bd8bf54d599357028c813
ice-myles <96409608+ice-myles@users.noreply.github.com>
2024-08-13 17:18:15 +0300

    mapstructure tag.

--- users/contract.go
+++ users/contract.go
@@ -308,7 +308,7 @@ type (
 		} `yaml:"globalAggregationInterval"`
 		//nolint:tagliatelle // .
 		IntervalBetweenRepeatableKYCSteps stdlibtime.Duration `yaml:"intervalBetweenRepeatableKYCSteps" mapstructure:"intervalBetweenRepeatableKYCSteps"`
-		MaxReferralsCount                 int64               `yaml:"maxReferralsCount"`
+		MaxReferralsCount                 int64               `yaml:"maxReferralsCount" mapstructure:"maxReferralsCount"`
 		DisableConsumer                   bool                `yaml:"disableConsumer"`
 	}
 )
