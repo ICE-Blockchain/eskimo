9a6a643b83b4ddc6f47fb8d3a9be4487e76c501e
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-10-22 17:27:56 +0300

    cross-tenant account linking package

---
+++ kyc/linking/linking.go
@@ -0,0 +1,180 @@
+// SPDX-License-Identifier: ice License 1.0
+
+package linking
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	stdlibtime "time"
+
+	"github.com/goccy/go-json"
+	"github.com/imroc/req/v3"
+	"github.com/pkg/errors"
+
+	"github.com/ice-blockchain/eskimo/users"
+	appcfg "github.com/ice-blockchain/wintr/config"
+	"github.com/ice-blockchain/wintr/connectors/storage/v2"
+	"github.com/ice-blockchain/wintr/log"
+	"github.com/ice-blockchain/wintr/server"
+	"github.com/ice-blockchain/wintr/time"
+)
+
+func init() { //nolint:gochecknoinits // It's the only way to tweak the client.
+	req.DefaultClient().SetJsonMarshal(json.Marshal)
+	req.DefaultClient().SetJsonUnmarshal(json.Unmarshal)
+	req.DefaultClient().GetClient().Timeout = requestDeadline
+	req.DefaultClient().SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+}
+
+func NewAccountLinker(ctx context.Context) Linker {
+	var cfg config
+	appcfg.MustLoadFromKey("kyc/linking", &cfg)
+	if len(cfg.TenantURLs) == 0 {
+		log.Panic("kyc/linking: Must provide tenantURLs")
+	}
+
+	return &linker{
+		globalDB: storage.MustConnect(ctx, ddl, ""),
+		cfg:      &cfg,
+	}
+}
+
+func (l *linker) Verify(ctx context.Context, now *time.Time, userID UserID, tokens map[Tenant]Token) (LinkedProfiles, error) {
+	res := map[Tenant]UserID{}
+	for tenant, token := range tokens {
+		remoteID, _, err := l.verifyToken(ctx, userID, tenant, token)
+		if err != nil {
+			return res, err
+		}
+		res[tenant] = remoteID
+	}
+	res[userID] = currentTenant(ctx)
+	if err := l.storeLinkedAccounts(ctx, now, userID, currentTenant(ctx), res); err != nil {
+		return nil, errors.Wrapf(err, "failed to save linked accounts for %v", userID)
+	}
+
+	return res, nil
+}
+
+func (l *linker) storeLinkedAccounts(ctx context.Context, now *time.Time, userID, tenant string, res map[Tenant]UserID) error {
+	params := []any{}
+	values := []string{}
+	idx := 1
+	for linkTenant, linkUserID := range res {
+		params = append(params, *now.Time, userID, tenant, linkTenant, linkUserID)
+		values = append(values, fmt.Sprintf("($%[1]v,$%[2]v,$%[3]v,$%[4]v,$%[5]v)", idx, idx+1, idx+2, idx+3, idx+4))
+		idx += 5
+	}
+	sql := fmt.Sprintf(`INSERT INTO 
+   									 linked_user_accounts(linked_at, tenant, user_id, linked_tenant, linked_user_id)
+    							VALUES %v`, strings.Join(values, ",\n"))
+	rows, err := storage.Exec(ctx, l.globalDB, sql, params...)
+	if err != nil {
+		return errors.Wrapf(err, "failed to save linked accounts for usr %v: %#v", userID, res)
+	}
+	if rows != uint64(len(res)) {
+		return errors.Errorf("failed unexpected rows on saving linked accounts for usr %v %v instead of %v", userID, rows, len(res))
+	}
+
+	return nil
+}
+func (l *linker) Get(ctx context.Context, userID UserID) (LinkedProfiles, error) {
+	linkedUsers, err := storage.Select[struct {
+		LinkedAt     *time.Time `db:"linked_at"`
+		UserID       string     `db:"user_id"`
+		Tenant       string     `db:"tenant"`
+		LinkedTenant string     `db:"linked_tenant"`
+		LinkedUserID string     `db:"linked_user_id"`
+	}](ctx, l.globalDB, `SELECT * FROM linked_user_accounts WHERE user_id = $1 OR linked_user_id = $1`, userID)
+	if err != nil && storage.IsErr(err, storage.ErrNotFound) {
+		return nil, errors.Wrapf(err, "failed to fetch linked accounts for user %v", userID)
+	}
+	res := make(map[Tenant]UserID)
+	for _, usr := range linkedUsers {
+		res[usr.LinkedTenant] = usr.LinkedUserID
+	}
+
+	return res, nil
+}
+
+func (l *linker) verifyToken(ctx context.Context, userID, tenant, token string) (remoteID UserID, hasFaceResult bool, err error) {
+	usr, err := l.fetchTokenData(ctx, tenant, token)
+	if err != nil {
+		if errors.Is(err, errRemoteUserNotFound) {
+			return "", false, ErrNotOwnRemoteUser
+		}
+		return "", false, errors.Wrapf(err, "failed to link accounts with %v", userID)
+	}
+	if usr.CreatedAt == nil && usr.ReferredBy != "" && usr.Username != "" {
+		return "", false, ErrNotOwnRemoteUser
+	}
+
+	return usr.ID, usr.HasFaceKYCResult(), nil
+}
+
+func (l *linker) fetchTokenData(ctx context.Context, tenant, token string) (*users.User, error) {
+	t, err := server.Auth(ctx).ParseToken(token, false)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid token passed")
+	}
+	if tenant != t.Tenant {
+		return nil, errors.Errorf("invalid token passed, %v does not match token %v", tenant, t.Tenant)
+	}
+	var resp *req.Response
+	var usr users.User
+	url, err := l.buildGetUserURL(t.Tenant, t.Subject)
+	if err != nil {
+		log.Panic(errors.Wrapf(err, "failed to detect tenant url"))
+	}
+	if resp, err = req.
+		SetContext(ctx).
+		SetRetryCount(10).                                                       //nolint:gomnd // .
+		SetRetryBackoffInterval(10*stdlibtime.Millisecond, 1*stdlibtime.Second). //nolint:gomnd // .
+		SetRetryHook(func(resp *req.Response, err error) {
+			if err != nil {
+				log.Error(errors.Wrap(err, "failed to check accounts linking, retrying... "))
+			} else {
+				body, bErr := resp.ToString()
+				log.Error(errors.Wrapf(bErr, "failed to parse negative response body for account linking"))
+				log.Error(errors.Errorf("failed check link accounts with status code:%v, body:%v, retrying... ", resp.GetStatusCode(), body))
+			}
+		}).
+		SetRetryCondition(func(resp *req.Response, err error) bool {
+			return err != nil ||
+				((resp.GetStatusCode() != http.StatusOK) && (resp.GetStatusCode() != http.StatusNotFound) && (resp.GetStatusCode() != http.StatusUnauthorized))
+		}).
+		SetHeader("Authorization", token).
+		AddQueryParam("caller", "eskimo-hut").
+		Get(url); err != nil {
+		return nil, errors.Wrap(err, "failed to link accounts")
+	} else if statusCode := resp.GetStatusCode(); statusCode != http.StatusOK {
+		if statusCode == http.StatusNotFound {
+			return nil, errRemoteUserNotFound
+		}
+
+		return nil, errors.Errorf("[%v]failed to link accounts", statusCode)
+	} else if data, err2 := resp.ToBytes(); err2 != nil {
+		return nil, errors.Wrapf(err2, "failed to read body of linking users: %v", url)
+	} else if jErr := json.UnmarshalContext(ctx, data, &usr); jErr != nil { //nolint:revive // .
+		return nil, errors.Wrapf(err2, "failed to decode body of linking users: %v", string(data))
+	}
+
+	return &usr, nil
+}
+
+func (l *linker) buildGetUserURL(tenant, userID string) (string, error) {
+	u, hasURL := l.cfg.TenantURLs[tenant]
+	if !hasURL {
+		return "", errors.Errorf("unknown tenant %v", tenant)
+	}
+
+	return url.JoinPath(u, "v1r/users/", userID)
+}
+
+func currentTenant(ctx context.Context) string {
+	return ctx.Value(tenantCtxKey).(string)
+}
